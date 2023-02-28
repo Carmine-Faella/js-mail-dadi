6e50e59fa2b7bf32a1fca0d70a7fc405b8ff4269
Carmine Faella <carminefaella5@gmail.com>
2023-02-28 10:28:42 +0100

    Correzioni

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,24 +1,66 @@
-let listaMail = ['mario@gmail.com', 'giovanni@gmail.com', 'barbara@hotmail.it', 'susan@hotmail.it'];
+/*let listaMail = ['mario@gmail.com', 'giovanni@gmail.com', 'barbara@hotmail.it', 'susan@hotmail.it'];
 
-let richiestaMail = prompt('Inserisci la tua email');
+let richiestaMail = document.getElementById('email');
 
 let accesso = document.getElementById('accesso');
 
-if(listaMail.values !== richiestaMail){
-    accesso.innerHTML = ('Accesso Negato');
-};
+let consentito = false;
 
 for(let i = 0; i<listaMail.length;i++){
-    if(listaMail[i] == richiestaMail){
-        accesso.innerHTML = ('Accesso consentito');
+
+    const email = listaMail[i];
+
+    if(email == richiestaMail){
+        consentito = true;
     }
 };
 
+if(consentito){
+    console.log('Accesso Consentito');
+}else{
+    console.log('Accesso Negato');
+};*/
 
+let listaMail = ['mario@gmail.com', 'giovanni@gmail.com', 'barbara@hotmail.it', 'susan@hotmail.it'];
 
+let richiestaMail = document.getElementById('email');
 
+let accesso = document.getElementById('accesso');
 
+const domButton = document.querySelector('#leggiForm');
 
+domButton.addEventListener('click', 
+    function (){
+
+        let consentito = false;
+
+        const email = richiestaMail.value;
+
+        for(let i = 0; i<listaMail.length;i++){
+
+            const mail = listaMail[i];
+        
+            if(mail == email){
+                consentito = true;
+            }
+        };
+        
+        if(consentito){
+            accesso.innerHTML = ('Accesso Consentito');
+        }else{
+            accesso.innerHTML = ('Accesso Negato');
+        };
+
+    }
+);
+
+const domReset = document.querySelector('#resettaForm');
+
+domReset.addEventListener('click', 
+    function() {  
+        richiestaMail.value = "";  
+    } 
+);
 
 
 
